a0fbb556f6b63523c64132c0166df8eead25dc52
Anton Shepelev <shepelev777@gmail.com>
2025-02-20 16:38:32 -0500

    Solution to Paint House III ( LeetCode 1473 ) Came cross this problem while I was practicing DP problems. It's a known problem with solutions posted online, but the implementation is mine - like the last problem, I'm going to do it on my own.
    Initially was going to propose Regular Expression Matching, but after scratching my head I was not able to solve it myself - only was able to come up with some ideas that were close to the solution, but not close enough to pass all test cases on LeetCode.
    
    Anyway, I think this 'Paint' problem is quite interesting implementation wise, so that's why I chose it. Feel free to take a look at the problem description, I will ping you for review here when I'm done with solving it.

---
+++ Paint House III/solution.h
@@ -0,0 +1,13 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+#include <vector>
+#include <span>
+#include "absl/log/check.h"
+
+// Returns a vector where each index [i] is the number of different nodes that
+// you can visit if you perform the process starting from some node [i].
+// Count Visited Nodes in a Directed Graph ( LeetCode 2876 )
+int CostToPaintNeighborhood(std::span<const int> cost, std::span<const int> houses, int m, int n, int target);
+
+#endif  // SOLUTION_H   
